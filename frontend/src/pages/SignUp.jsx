ed958bc69c4b1b692c47d382b81467a31af5aced
Deep <majumderdeep06@gmail.com>
2025-07-03 14:54:44 +0530

    Tested login,logout and signup routes,,started signup ui

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='w-full h-[100vh] bg-slate-200 flex items-center justify-center'>
+      <div className='w-full max-w-[400px] h-[500px] bg-white rounded-lg shadow-gray-400 shadow-lg
+      flex flex-col gap-[10px]'>
+        <div className='w-full h-[200px] bg-[#20c7ff] rounded-b-[30%] shadow-gray-400 shadow-lg
+        flex items-center justify-center'>
+          <h1 className='text-gray-600 font-bold text-[19px]'>Welcome to Convo</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
